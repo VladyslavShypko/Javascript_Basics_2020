6c8eff6a6d003a5e589715b3751e2066d4228b17
Vladyslav Shypko <58210005+VladyslavShypko@users.noreply.github.com>
2020-12-19 13:23:59 +0200

    add .eslintrc

---
+++ Final-task/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	"extends": "eslint:recommended",
+	"env": {
+		"browser": true,
+		"es6": true
+	},
+	"rules": {
+	},
+	"parserOptions": {
+		
+		"sourceType": "module",
+		"ecmaVersion": 9,
+    }
+}
